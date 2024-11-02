80b9d8a976cc9df14a8d9d7f4eed5ebb380d6df9
Kanagalingam Senthalan <senthalan@users.noreply.github.com>
2024-11-02 21:16:55 +0530

    Fix compile issue

--- hello-world-nodejs-task/index.js
+++ hello-world-nodejs-task/index.js
@@ -6,9 +6,19 @@ const apiKey =  process.env.API_KEY;
 console.log('Hello World!');
 console.log(serviceURL);
 console.log(apiKey);
-const response = await axios.get(serviceURL + '/greeting', {
-    headers: {
-      'Choreo-API-Key': apiKey
-    }
-});
-console.log(response);
+
+async function fetchData() {
+  try {
+    const response = await axios.get(serviceURL + '/greeting', {
+      headers: {
+        'Choreo-API-Key': apiKey
+      }
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+// Call the async function
+fetchData();
